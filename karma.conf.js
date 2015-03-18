4f54aa68847972cbb66ede087a040ade9c494d26
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-03-18 00:10:03 -0400

    Updates "shelljs" dependency
    Updates "shelljs" dependency from 0.3.0 to 0.4.0

--- karma.conf.js
+++ karma.conf.js
@@ -12,8 +12,8 @@ module.exports = function (config) {
             'app/bower_components/angular-route/angular-route.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
 
-            'app/components/**/*.js',
-            'app/view*/**/*.js',
+            //'app/components/**/*.js',
+            //'app/view*/**/*.js',
 
             'lib/**/*.js',
             'test/unit/**/*.js'
