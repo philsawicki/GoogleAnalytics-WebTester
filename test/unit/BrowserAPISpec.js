9b1a9104a2b4451e92d009808b96db2608e5b186
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-03-21 22:04:15 -0400

    Adds utility methods to the GAWebTester object
    Adds utility methods to the GAWebTester object.

--- test/unit/BrowserAPISpec.js
+++ test/unit/BrowserAPISpec.js
@@ -5,18 +5,26 @@ describe('The Browser API', function () {
 	var module;
 
 	beforeEach(function () {
-        module = window.GoogleAnalyticsWebTester;
+        module = window.GoogleAnalyticsWebTesterBrowserAPI;
     });
 
 
     describe('The Module interface', function () {
+        it('exposes the "installDataInterceptor" method', function () {
+            expect( module.installDataInterceptor ).toBeDefined();
+        });
+
         it('exposes the "uninstallDataInterceptor" method', function () {
-            expect( module.initialize ).toBeDefined();
+            expect( module.uninstallDataInterceptor ).toBeDefined();
         });
     });
 
 
     describe('The "window namespace" of the Module', function () {
+        it('exposes the "installDataInterceptor" method', function () {
+            expect( window.GoogleAnalyticsWebTesterBrowserAPI.installDataInterceptor ).toBeDefined();
+        });
+
         it('exposes the "uninstallDataInterceptor" method', function () {
             expect( window.GoogleAnalyticsWebTesterBrowserAPI.uninstallDataInterceptor ).toBeDefined();
         });
@@ -26,6 +34,50 @@ describe('The Browser API', function () {
     describe('The proper "etiquette" of the Module', function () {
         it('does not leak the "uninstallDataInterceptor" method to the "window"', function () {
             expect( window.uninstallDataInterceptor ).not.toBeDefined();
+        });
+    });
+
+
+    describe('The "window.GAWebTester" API', function () {
+        beforeEach(function () {
+            module.installDataInterceptor();
+        });
+
+        afterEach(function () {
+            module.uninstallDataInterceptor();
+        });
+
+        it('should have deployed "window.GAWebTester"', function () {
+            expect( window.GAWebTester ).toBeDefined();
+            expect( window.GAWebTester ).not.toBeNull();
+        });
+
+        describe('The "getData()" method', function () {
+            it('should return a default object when no Event fired', function () {
+                expect( window.GAWebTester.getData() ).toBeDefined();
+                expect( window.GAWebTester.getData() ).not.toBeNull();
+                expect( window.GAWebTester.getData() ).toEqual({
+                    EventBuffer: [],
+                    LastEvent: {},
+                    GTMTrackerName: null
+                });
+            });
+        });
+
+        describe('The "getEventBuffer()" method', function () {
+            it('should return an empty Array when no Event recorded', function () {
+                expect( window.GAWebTester.getEventBuffer() ).toBeDefined();
+                expect( window.GAWebTester.getEventBuffer() ).not.toBeNull();
+                expect( window.GAWebTester.getEventBuffer() ).toEqual([]);
+            });
+        });
+
+        describe('The "getLastEvent()" method', function () {
+            it('should return an empty Object when no Event recorded', function () {
+                expect( window.GAWebTester.getLastEvent() ).toBeDefined();
+                expect( window.GAWebTester.getLastEvent() ).not.toBeNull();
+                expect( window.GAWebTester.getLastEvent() ).toEqual({});
+            });
         });
     });
 });
