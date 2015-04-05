b24a3e0b09d35d559ed4267ce0d0f2d0cbeb4b4c
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-04-04 23:19:37 -0400

    Updates site with new layout
    Updates site with new layout.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp    = require('gulp'),
+    jade    = require('gulp-jade'),
+    data    = require('gulp-data')
+    rename  = require('gulp-rename'),
+    sitemap = require('gulp-sitemap'),
+    path    = require('path'),
+    fs      = require('fs');
+
+
+gulp.task('jade', function () {
+    return gulp.src('./templates/**/*.page.jade')
+        //.pipe(plumber())
+        .pipe(data(function (file) {
+            return require('./data/' + path.basename(file.path) + '.json');
+        }))
+        .pipe(jade({
+            pretty: true,
+            locals: require('./data/siteData.json')
+        }))
+        .pipe(rename({ extname: '' }))
+        .pipe(rename({ extname: '.html' }))
+        //.pipe(sitemap({
+        //    siteUrl: 'https://philsawicki.github.io/GoogleAnalytics-WebTester'
+        //}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('sitemap', function () {
+    return gulp.src('./templates/**/*.page.jade')
+        //.pipe(plumber())
+        .pipe(data(function (file) {
+            return require('./data/' + path.basename(file.path) + '.json');
+        }))
+        .pipe(jade({
+            pretty: true,
+            locals: require('./data/siteData.json')
+        }))
+        .pipe(rename({ extname: '' }))
+        .pipe(rename({ extname: '.html' }))
+        .pipe(sitemap({
+            siteUrl: 'https://philsawicki.github.io/GoogleAnalytics-WebTester'
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./templates/**/*.jade', ['jade']);
+    gulp.watch('./templates/**/*.page.jade', ['sitemap']);
+});
+
+gulp.task('default', ['watch'], function (callback) {
+    callback();
+});
