f6255e3e00ce07d42c338c54395e99bfc2cfe6b0
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-04-05 20:45:37 -0400

    Loads JavaScript async for better load time
    Loads JavaScript async for better performance load time.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp       = require('gulp'),
     minifyHTML = require('gulp-minify-html'),
     rename     = require('gulp-rename'),
     sitemap    = require('gulp-sitemap'),
+    uglify     = require('gulp-uglify'),
     path       = require('path'),
     fs         = require('fs');
 
@@ -45,9 +46,18 @@ gulp.task('sitemap', function () {
         .pipe(gulp.dest('./'));
 });
 
+gulp.task('js', function () {
+    return gulp.src('./javascripts/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({ extname: '' }))
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('./public/js/'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('./templates/**/*.jade', ['jade']);
     gulp.watch('./templates/**/*.page.jade', ['sitemap']);
+    gulp.watch('./javascripts/**/*.js', ['js']);
 });
 
 gulp.task('default', ['watch'], function (callback) {
