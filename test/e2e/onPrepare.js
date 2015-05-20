cb1361cb84e5718030f288266f1e33991afadc98
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-05-19 23:16:58 -0400

    Add utility methods to take screenshots
    Add utility methods to take screenshots of passing/failing tests.

--- test/e2e/onPrepare.js
+++ test/e2e/onPrepare.js
@@ -18,3 +18,48 @@ beforeEach(function () {
         browserDriver: browser.driver  // A reference to Protractor's WebDriver.
     });
 });
+
+
+/**
+ * Take screenshots of passing/failing tests.
+ */
+if (false) {
+    var fs = require('fs');
+
+    var Utils = Utils || {
+        /**
+         * @name screenShotDirectory
+         * @description The directory where screenshots will be created
+         * @type {String}
+         */
+        screenShotDirectory: 'test/',
+
+        /**
+         * @name writeScreenShot
+         * @description Write a screenshot string to file.
+         * @param {String} data The base64-encoded string to write to file
+         * @param {String} filename The name of the file to create (do not specify directory)
+         */
+        writeScreenShot: function (data, filename) {
+            var stream = fs.createWriteStream(this.screenShotDirectory + filename);
+
+            stream.write(new Buffer(data, 'base64'));
+            stream.end();
+        }
+    };
+
+    afterEach(function () {
+        var passed = jasmine.getEnv().currentSpec.results().passed();
+        var currentSpec = jasmine.getEnv().currentSpec;
+
+        browser.takeScreenshot().then(function (png) {
+            browser.getCapabilities().then(function (capabilities) {
+                var browserName = capabilities.caps_.browserName;
+                var passFail = (passed) ? 'pass' : 'FAIL';
+                var filename = browserName + ' ' + passFail + ' - ' + currentSpec.id + '.png';
+
+                Utils.writeScreenShot(png, filename);
+            });
+        });
+    });
+}
